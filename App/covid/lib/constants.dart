426d7cbcd52878d9457d1dd3c9c7ba2c6d735401
janmejai2002 <53612892+janmejai2002@users.noreply.github.com>
2020-08-19 21:37:26 +0530

    Added more features in app

--- App/covid/lib/constants.dart
+++ App/covid/lib/constants.dart
@@ -7,3 +7,42 @@ const kHomeScreenTextStyle = TextStyle(
     fontSize: 25);
 
 const kHomeScreenIconSize = 40.0;
+
+final kBottomBarTextStyle =
+    TextStyle(fontFamily: 'Baloo', color: Colors.black, fontSize: 20.0);
+
+final kSymptomCardTitleTextStyle = TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+
+final kSymptomCardInfoStyle =
+    TextStyle(fontFamily: 'Baloo', fontSize: 17, height: 1.2);
+
+final kAppBarStyle = TextStyle(
+    color: Colors.black,
+    fontFamily: 'Poppins',
+    fontSize: 25.0,
+    fontWeight: FontWeight.bold);
+
+final kactionNameGreenStyle = TextStyle(
+  color: Color(0xFF3B978E),
+  fontFamily: 'Poppins',
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+
+final kactionNameRedStyle = TextStyle(
+  color: Color(0xFFD84B6B),
+  fontFamily: 'Poppins',
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+
+final kactionInfoStyle = TextStyle(
+  color: Colors.black,
+  fontFamily: 'Ballo',
+  fontSize: 17,
+  height: 1.2,
+);
--- App/covid/lib/screens/home_page.dart
+++ App/covid/lib/screens/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:covid/constants.dart';
+import 'package:covid/screens/symptoms.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:covid/components/reusable_card.dart';
+import 'package:covid/screens/dos_and_donts.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,74 +11,159 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  void onTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  final List<Widget> _children = [
+    HomeBase(),
+    Cases(),
+    Predict(),
+  ];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Expanded(
-              flex: 1,
-              child: Container(
-                color: Color(0xFF),
-                padding: EdgeInsets.only(left: 20.0, top: 20.0),
-                child: Text(
-                  'Home',
-                  style: TextStyle(
-                    fontFamily: 'Baloo',
-                    fontSize: 40.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: onTapped,
+          backgroundColor: Color(0xFFDFE2E2),
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                FontAwesomeIcons.home,
+                color: Colors.black,
+              ),
+              title: Text(
+                'Home',
+                style: kBottomBarTextStyle,
               ),
             ),
-            Expanded(
-              flex: 2,
-              child: HomeScreenCard(
-                  colour: Color(0xFF47B2A0),
-                  onPress: () {},
-                  iconChild: Icon(
-                    FontAwesomeIcons.lungsVirus,
-                    size: kHomeScreenIconSize,
-                  ),
-                  text: 'Symptoms'),
+            BottomNavigationBarItem(
+              icon: Icon(
+                FontAwesomeIcons.chartLine,
+                color: Colors.black,
+              ),
+              title: Text(
+                'Cases',
+                style: kBottomBarTextStyle,
+              ),
             ),
-            Expanded(
-              flex: 2,
-              child: HomeScreenCard(
-                  colour: Color(0xFFF9D553),
-                  onPress: () {},
-                  iconChild: Icon(FontAwesomeIcons.handsWash,
-                      size: kHomeScreenIconSize),
-                  text: 'Do\'s and Dont\'s'),
-            ),
-            Expanded(
-              flex: 2,
-              child: HomeScreenCard(
-                  colour: Color(0xFFF48A8F),
-                  onPress: () {},
-                  iconChild: Icon(
-                    FontAwesomeIcons.virus,
-                    size: kHomeScreenIconSize,
-                  ),
-                  text: 'Virus'),
-            ),
-            Expanded(
-              flex: 2,
-              child: HomeScreenCard(
-                  colour: Color(0xFF77BBF3),
-                  onPress: () {},
-                  iconChild: Icon(
-                    FontAwesomeIcons.bookOpen,
-                    size: kHomeScreenIconSize,
-                  ),
-                  text: 'Myth'),
-            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                FontAwesomeIcons.superpowers,
+                color: Colors.black,
+              ),
+              title: Text(
+                'Predict',
+                style: kBottomBarTextStyle,
+              ),
+            )
           ],
         ),
+        body: _children[_currentIndex],
       ),
+    );
+  }
+}
+
+class HomeBase extends StatelessWidget {
+  const HomeBase({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: Container(
+            color: Color(0xFF),
+            padding: EdgeInsets.only(left: 20.0, top: 20.0),
+            child: Text(
+              'Home',
+              style: TextStyle(
+                fontFamily: 'Baloo',
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: HomeScreenCard(
+              colour: Color(0xFF47B2A0),
+              onPress: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SymptomsPage()));
+              },
+              iconChild: Icon(
+                FontAwesomeIcons.lungsVirus,
+                size: kHomeScreenIconSize,
+              ),
+              text: 'Symptoms'),
+        ),
+        Expanded(
+          flex: 2,
+          child: HomeScreenCard(
+              colour: Color(0xFFF9D553),
+              onPress: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => DosAndDontsPage()));
+              },
+              iconChild:
+                  Icon(FontAwesomeIcons.handsWash, size: kHomeScreenIconSize),
+              text: 'Do\'s and Dont\'s'),
+        ),
+        Expanded(
+          flex: 2,
+          child: HomeScreenCard(
+              colour: Color(0xFFF48A8F),
+              onPress: () {},
+              iconChild: Icon(
+                FontAwesomeIcons.virus,
+                size: kHomeScreenIconSize,
+              ),
+              text: 'Virus'),
+        ),
+        Expanded(
+          flex: 2,
+          child: HomeScreenCard(
+              colour: Color(0xFF77BBF3),
+              onPress: () {},
+              iconChild: Icon(
+                FontAwesomeIcons.bookOpen,
+                size: kHomeScreenIconSize,
+              ),
+              text: 'Myth'),
+        ),
+      ],
+    );
+  }
+}
+
+class Cases extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+    );
+  }
+}
+
+class Predict extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orange,
     );
   }
 }
