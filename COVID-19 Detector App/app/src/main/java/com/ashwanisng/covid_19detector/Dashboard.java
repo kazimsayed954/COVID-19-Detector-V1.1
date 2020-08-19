db6d4661c6892b24871b16ea78f6cb1ed7ecc517
ashwanisng <ashwanicena5@gmail.com>
2020-08-20 01:02:30 +0530

    splash screen added

---
+++ COVID-19 Detector App/app/src/main/java/com/ashwanisng/covid_19detector/Dashboard.java
@@ -0,0 +1,14 @@
+package com.ashwanisng.covid_19detector;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
